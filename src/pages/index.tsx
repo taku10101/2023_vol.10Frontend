a96c6bfb2ce8880d3fcebd08b03e634d28f2e8d9
taku10101 <taku1011ht@gmail.com>
2023-10-01 01:23:29 +0900

    indexfix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,9 @@ import Header from "@/components/Header";
 import React from "react";
 
 export default function index() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
