001dd971c1be5cadade1aaf89d4d728c7476ceb9
taku10101 <taku1011ht@gmail.com>
2023-10-03 18:05:23 +0900

    postuserfix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,54 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+
+import "firebase/auth";
 
 //post put
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
 
 type Data = {
   name: string;
+  uid: string;
+  email: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+const getUser = async () => {
+  const firebaseUser = auth.currentUser;
+  if (!firebaseUser) return null;
+  const response = await fetch(
+    `http://172.168.0.0.1:8080/users/${firebaseUser.uid}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const getUser = await response.json();
+  console.log(getUser);
+  return getUser;
+};
+
+export async function postUser() {
+  const user = auth.currentUser;
+  if (!getUser) {
+    const response = await fetch("http://127.0.0.1:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: user?.uid,
+        name: user?.displayName,
+        // email: user?.email,
+        // photoURL: user?.photoURL,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } else {
+    console.log("user already exists");
+  }
 }
