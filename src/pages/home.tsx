affbc5647440b4f2b21b816ba48c085ab122defa
taku10101 <taku1011ht@gmail.com>
2023-10-05 17:31:10 +0900

    fixeditor

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,16 @@
 import Header from "../components/Header";
+import { useState } from "react";
 import RequireAuth from "../components/common/RequireAuth";
 import Editor from "../components/editor/Editor";
 import React from "react";
 
 export default function Home() {
+  const [code, setCode] = useState<string>("console.log('Hello, world!');");
   return (
     <>
       <Header />
       <RequireAuth>
-        <Editor />
+        <Editor value={code} onChange={(newCode) => setCode(newCode)} />
       </RequireAuth>
     </>
   );
