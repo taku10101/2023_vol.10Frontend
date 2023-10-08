913dc09d92a393e561beace96cb57e50680e7f20
taku10101 <taku1011ht@gmail.com>
2023-10-08 22:12:21 +0900

    websoketFix

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,40 +1,43 @@
-import { Box } from "@mui/material";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactAce from "react-ace/lib/ace";
-import { io } from "socket.io-client";
 
 const Editor: React.FC = () => {
   const [text, setText] = useState<string>("");
-  const [socket, setSocket] = useState<any | null>(null);
-
+  const [socket, setSocket] = useState<WebSocket | null>(null);
   useEffect(() => {
-    // Socket.ioのインスタンスを初期化
-    const ioInstance = io("");
-    setSocket(ioInstance);
+    const ws = new WebSocket("ws://your-websocket-server-url");
 
-    // 初期データの取得
-    ioInstance.on("initialize", (data: string) => {
-      setText(data);
-    });
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+    };
 
-    // 他のクライアントからの更新を受け取る
-    ioInstance.on("update", (data: string) => {
-      setText(data);
-    });
+    // WebSocketのコネクションが閉じたら、コネクションを破棄
+    ws.onmessage = (event) => {
+      console.log("WebSocket message received:", event);
+      console.log("WebSocket message received:", event.data);
+      // 他のクライアントからの変更内容を受け取り、表示内容を更新
+      setText(event.data);
+    };
 
-    // コンポーネントのクリーンアップ時にソケット接続を終了
+    ws.onclose = () => {
+      console.log("WebSocket disconnected");
+      setSocket(null);
+    };
+
+    setSocket(ws);
+
     return () => {
-      ioInstance.disconnect();
+      ws.close();
     };
   }, []);
 
+  useEffect(() => {}, [text]);
+
   const handleChangeText = (value: string, e?: any) => {
     setText(value);
-    // テキストの変更をサーバーに送信
-    if (socket) {
-      socket.emit("edit", value);
-    }
   };
+
+  console.log(text);
 
   return (
     <ReactAce
