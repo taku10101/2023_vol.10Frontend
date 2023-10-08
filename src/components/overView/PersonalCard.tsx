0da31bf64a089f12b4400e361e65d80dc10fad30
taku10101 <taku1011ht@gmail.com>
2023-10-08 23:25:12 +0900

    overviewレイアウトFix

---
+++ src/components/overView/PersonalCard.tsx
@@ -0,0 +1,55 @@
+import {
+  CardContent,
+  CardMedia,
+  Card,
+  Grid,
+  Typography,
+  Paper,
+} from "@mui/material";
+import React from "react";
+
+export const PersonalCard = () => {
+  return (
+    <>
+      <Typography variant='h3' sx={{ ml: 2 }}>
+        Personal
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: "20px",
+        }}
+      >
+        {[1, 2, 3, 4].map((item, index, detail) => (
+          <Grid item xs={3} key={index}>
+            <Paper
+              elevation={5}
+              style={{ borderRadius: "8px", backgroundColor: "white" }}
+            >
+              {/* Image for the card */}
+              <CardMedia
+                component='img'
+                height='150'
+                image='https://source.unsplash.com/random'
+                alt={`Card Image ${item}`}
+                style={{
+                  borderTopLeftRadius: "8px",
+                  borderTopRightRadius: "8px",
+                }}
+              />
+              <CardContent>
+                <Typography variant='h6' gutterBottom>
+                  Card Title {item}
+                </Typography>
+                <Typography variant='body2' color='textSecondary'>
+                  detail:{item}
+                </Typography>
+              </CardContent>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
---
+++ src/components/overView/TeamCard.tsx
@@ -0,0 +1,48 @@
+import { CardContent, CardMedia, Grid, Typography, Paper } from "@mui/material";
+import React from "react";
+
+export const TeamCard = () => {
+  return (
+    <>
+      <Typography variant='h3' sx={{ ml: 2 }}>
+        Team
+      </Typography>
+
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: "20px",
+        }}
+      >
+        {[1, 2, 3, 4].map((item, index, detail) => (
+          <Grid item xs={3} key={index}>
+            <Paper
+              elevation={5}
+              style={{ borderRadius: "8px", backgroundColor: "white" }}
+            >
+              <CardMedia
+                component='img'
+                height='150'
+                image='https://source.unsplash.com/random'
+                alt={`Card Image ${item}`}
+                style={{
+                  borderTopLeftRadius: "8px",
+                  borderTopRightRadius: "8px",
+                }}
+              />
+              <CardContent>
+                <Typography variant='h6' gutterBottom>
+                  Card Title {item}
+                </Typography>
+                <Typography variant='body2' color='textSecondary'>
+                  detail:{item}
+                </Typography>
+              </CardContent>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
