48653ac9bf58c4d9649a8d12e1b309eb2c431377
taku10101 <taku1011ht@gmail.com>
2023-10-14 12:20:37 +0900

    getProjectdel

--- src/components/api/project.ts
+++ src/components/api/project.ts
@@ -1,26 +1,45 @@
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
+import { auth } from "../../lib/firebase/client";
+export const createProject = async () => {
+  const firebaseUser = auth.currentUser;
+  if (!firebaseUser) return null;
+  const response = await fetch(
+    `http://localhost:8080/users/${firebaseUser.uid}/projects`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "test",
+      }),
+    }
+  );
+  const data = await response.json();
 
-const auth = getAuth();
-
-export const createProject = async (id: string) => {
-  const getUser = async () => {
-    const firebaseUser = auth.currentUser;
-    if (!firebaseUser) return null;
-    const response = await fetch(
-      `http://localhost:8080/users/${firebaseUser.uid}/projects`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: "test",
-        }),
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
+  console.log(data);
+  return Promise.resolve(data);
 };
+
+// export const getProjectsById = async () => {
+//   const newProject = await createProject();
+//   const project_id = newProject.project_id;
+
+//   const firebaseUser = auth.currentUser;
+//   if (!firebaseUser) return null;
+
+//   const response = await fetch(
+//     `http://localhost:8080/users/${firebaseUser.uid}/projects/${project_id}`,
+//     {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
+//   const data = await response.json();
+
+//   console.log(data);
+//   return Promise.resolve(data);
+// };
